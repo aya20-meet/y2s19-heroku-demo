3394020c05705ccfc7fdda574eca6349ecc8dc18
myname21-meet <myname21@meet.mit.edu>
2019-07-31 06:44:33 -0400

    Added my flask templates work

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World test!'
-
+def home_page():
+	fave=["Burger","Pasta","pizza"]
+	not_food=["yams","menta","yasa"]
+	return render_template("index.html", fave=fave, not_food=not_food, yuval=False)
+@app.route('/singup')
+def singup_page():
+    return render_template("singup.html","")
+@app.route('/thankyou')
+def thankyou():
+    return render_template("thankyou.html")
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug = True, port=2000)
